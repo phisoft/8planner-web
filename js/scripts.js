94bf516cb959a2d0ee62754063ac1840e3b05b82
rosclaritha <rosclaritha@gmail.com>
2022-06-15 10:47:34 +0800

    update meta data

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,3 @@
-/* Template: Sync - Free Mobile App Landing Page HTML Template
-   Author: Inovatik
-   Created: Dec 2019
-   Description: Custom JS file
-*/
-
-
-
 (function ($) {
     "use strict";
 
